0c04df2c9d57b24d775a0ae6f460b2dae402f388
Corey Wettlaufer <corey.wettlaufer@gmail.com>
2018-06-24 12:35:00 -0400

    font-page layout changes

--- wpbootstrap/front-page.php
+++ wpbootstrap/front-page.php
@@ -1,4 +1,50 @@
 <!-- This is the code structure for the front page. If you need to change the structure/layout of the front page plese create a front-page.php in your child theme. -->
 <?php get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<!--
+	   Use this section if you have a static featured image at the top of your page with text.
+	-->
 
+	<?php
+		if(has_post_thumbnail()) {
+			echo '<div class="mast page-mast">';
+				the_post_thumbnail();
+
+				$mast_title       = get_post_meta( get_the_ID(), 'mast_title', true );
+	            $mast_description = get_post_meta( get_the_ID(), 'mast_description', true );
+
+				if( empty($mast_title) ) {
+	                $mast_title =  the_title('<h1>', '</h1>', false);
+	            }
+
+				echo '<div class="container mast-overlay">';
+					echo '<h1>'.$mast_title.'</h1>';
+
+					if( !empty($mast_description) ) {
+						echo wpautop( $mast_description );
+					}
+				echo '</div>'; // End of mast-overlay container
+			echo '</div>'; // End of page-mast container
+		}
+	 ?>
+
+
+   <!--
+      Use this section if you have a static featured image at the top of your page with NO text
+   -->
+   <?php
+      /*if(has_post_thumbnail) {
+         ?>
+         <div class="container-fluid padding0 featuredImage">
+            <?php //the_post_thumbnail(); ?>
+         </div>
+         <?php
+      }*/
+   ?>
+
+    <div id="fullWidth">
+        <?php the_content(); ?>
+    </div>
+
+<?php endwhile; // End of the loop. ?>
 <?php get_footer(); ?>
