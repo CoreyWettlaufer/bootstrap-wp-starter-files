b1d283840dae177b990e6fdcc4cad2567b5d52ef
Corey Wettlaufer <corey.wettlaufer@gmail.com>
2018-06-24 10:31:39 -0400

    front page and page

---
+++ wpbootstrap/front-page.php
@@ -0,0 +1,1 @@
+<!-- This is the code structure for the front page. If you need to change the structure/layout of the front page plese create a front-page.php in your child theme. -->
---
+++ wpbootstrap/page.php
@@ -0,0 +1,1 @@
+<!-- This is the code structure for the default sub-pages. If you need to change the structure/layout of the page plese create a page.php in your child theme. -->
