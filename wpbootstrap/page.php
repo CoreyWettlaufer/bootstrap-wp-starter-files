01d1b622f2e7e1cdfdde8c1c522fe762d6d78a5c
Corey Wettlaufer <corey.wettlaufer@gmail.com>
2018-06-24 12:15:49 -0400

    remove unnecessary stuff from page.php

--- wpbootstrap/page.php
+++ wpbootstrap/page.php
@@ -29,34 +29,6 @@
 		}
 	 ?>
 
-	 <!--
-  	<div class="mast page-mast">
-  	    <?php/*
-      	    if( has_post_thumbnail() ) {
-      	        the_post_thumbnail();
-      	    } else {
-      	        echo '<img src="'. home_url() . '/wp-content/uploads/2018/01/Banner5.jpg" />';
-      	    }
-
-              $mast_title       = get_post_meta( get_the_ID(), 'mast_title', true );
-              $mast_description = get_post_meta( get_the_ID(), 'mast_description', true );
-
-              if( empty($mast_title) ) {
-                  $mast_title =  the_title('<h1>', '</h1>', false);
-              }
-  	    ?>
-          <div class="container mast-overlay">
-              <?php
-                  echo $mast_title;
-
-                  if( !empty($mast_description) ) {
-                      echo wpautop( $mast_description );
-                  }
-              ?>
-          </div>
-  	</div>
--->
-
 
    <!--
       Use this section if you have a static featured image at the top of your page with NO text
